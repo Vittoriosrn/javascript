2898ad3a36965bf9dedbc9eba1bf556f4b5bc87e
Vittorio Kevin <95937607+Vittoriosrn@users.noreply.github.com>
2023-05-31 08:02:47 -0300

    Initial commit

---
+++ aula004/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if(vel > 50){
+    console.log(`Você ultrapassou a velocidade permitida de 50 km/h`)
+    console.log(`Voçê foi multado!`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
---
+++ de001/script.js
@@ -0,0 +1,8 @@
+var agora = new Date()
+var horas = agora.getHours() 
+var horastxt = window.document.querySelector("p#horas")
+horastxt.innerHTML = `<p>Agora são ${horas} horas</p>`
+
+if(horas < 12){
+window.document.querySelector("")
+}
