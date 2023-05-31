4dde58eb2b9128128d7a42504e00f725291df520
Vittorio Kevin <95937607+Vittoriosrn@users.noreply.github.com>
2023-05-31 09:00:38 -0300

    atualização no desafio1

--- de001/script.js
+++ de001/script.js
@@ -1,8 +1,15 @@
 var agora = new Date()
-var horas = agora.getHours() 
+var horas = agora.getHours()
 var horastxt = window.document.querySelector("p#horas")
 horastxt.innerHTML = `<p>Agora são ${horas} horas</p>`
 
-if(horas < 12){
-window.document.querySelector("")
+if(horas > 4 && horas < 12){
+    window.document.querySelector("img").src = "img/manha.png"
+    window.document.body.style.backgroundColor = "#edd7c2"
+}else if(horas < 19 && horas > 11){
+    window.document.querySelector("img").src = "img/tarde.png"
+    window.document.body.style.backgroundColor = "#c5d4df"
+}else if(horas > 18 || horas < 5){
+    window.document.querySelector("img").src = "img/noite.png"
+    window.document.body.style.backgroundColor = "#242d46"
 }
