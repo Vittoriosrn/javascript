d8e282eb220f85f7dfcd6eadf32b1eeb82ab91b6
Vittorio Kevin <95937607+Vittoriosrn@users.noreply.github.com>
2023-06-01 07:50:33 -0300

    aula006 adicionada

---
+++ aula006/estruturas_repeticao.js
@@ -0,0 +1,26 @@
+// Contando de 1 até 10 utilizando FOR
+
+console.log(`Utlizando o for()`)
+for(var i=1; i <= 10; i++){
+    console.log(`Contador: ${i}`);
+}
+
+i = 1
+// Contando de 1 até 10 utilizando WHILE
+
+console.log(`\nUtlizando o while()`)
+
+while(i <= 10){
+    console.log(`Contador: ${i}`);
+    i++
+}
+
+i = 1
+// Contando 1 até 10 utilizando DO-WHILE
+
+console.log(`\nUtlizando o do{}while()`)
+
+do{
+    console.log(`Contador: ${i}`);
+    i++
+}while(i <= 10)
